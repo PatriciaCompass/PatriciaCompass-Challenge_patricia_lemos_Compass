304872cb646c7f88dd4c9c0b19e6c9c72dbf07be
PatriciaCompass <patricia.lemos@compasso.com.br>
2022-09-10 14:03:40 -0300

    Atualizando e modificando steps

--- features/step_definitions/busca.rb
+++ features/step_definitions/busca.rb
@@ -4,12 +4,15 @@ Dado('que esteja na home') do
 end
   
 Quando('realizar uma busca por um produto existente') do
-   @home_page.search_for('SPEAKERS')
+   product_for_search = Factory::Static.static_data('valid_product')
+   @home_page.search_for(product_for_search)
    @search_results_page = Pages::SearchResults.new
 end
   
 Então('deverão ser retornados resultados na busca') do
-  expect(@search_results_page).to have_products
+  expect(@search_results_page).not_to have_products
+  expect(@search_results_page.products.first.all_there?).to be_truthy
+ 
 end
 
 Quando('buscar pelo produto {string}') do |product|                            
